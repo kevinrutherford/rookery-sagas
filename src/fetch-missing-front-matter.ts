5933c503dcbe87e4deb1808847423c2b63c65660
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-08 14:26:05 +0100

    Patch the first undetermined DOI to be not-found #3

--- src/fetch-missing-front-matter.ts
+++ src/fetch-missing-front-matter.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import * as E from 'fp-ts/Either'
+import * as O from 'fp-ts/Option'
+import * as RA from 'fp-ts/ReadonlyArray'
 import * as T from 'fp-ts/Task'
 import * as TE from 'fp-ts/TaskEither'
 import { flow, pipe } from 'fp-ts/function'
@@ -16,6 +18,30 @@ const workResponse = t.type({
     }),
   })),
 })
+
+type WorkResponse = t.TypeOf<typeof workResponse>
+
+const recordNotFound = (logger: L.Logger) => (work: WorkResponse['data'][number]) => pipe(
+  TE.tryCatch(
+    async () => axios.patch(`http://commands:44001/works/${work.id}`, {
+      data: {
+        type: work.type,
+        id: work.id,
+        attributes: {
+          crossrefStatus: 'not-found',
+        },
+      },
+    }, {
+      headers: { 'Content-Type': 'application/json' },
+    }),
+    (error) => logger.error('failed to update work', {
+      url: `http://commands:44001/works/${work.id}`,
+      work,
+      error,
+    }),
+  ),
+  TE.map(() => logger.info('work updated')),
+)
 
 export const fetchMissingFrontMatter = async (logger: L.Logger): Promise<void> => {
   logger.info('fetchMissingFrontMatter starting')
@@ -34,6 +60,12 @@ export const fetchMissingFrontMatter = async (logger: L.Logger): Promise<void> =
     TE.chainEitherK(flow(
       workResponse.decode,
       E.mapLeft((errors) => logger.error('invalid response', { url, errors: formatValidationErrors(errors) })),
+    )),
+    TE.map((res) => res.data),
+    TE.map(RA.head),
+    TE.chainW(O.match(
+      () => TE.right<void, undefined>(undefined),
+      recordNotFound(logger),
     )),
     T.map(() => logger.info('fetchMissingFrontMatter finished')),
   )()
