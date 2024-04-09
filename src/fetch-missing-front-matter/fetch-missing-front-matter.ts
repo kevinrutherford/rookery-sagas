610e5982b76464e90f6c05650e10ccfd0ed0f771
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-09 09:43:25 +0100

    Remove some duplication #3

--- src/fetch-missing-front-matter/fetch-missing-front-matter.ts
+++ src/fetch-missing-front-matter/fetch-missing-front-matter.ts
@@ -8,27 +8,26 @@ import { fetchWorks } from './fetch-works'
 import { Work } from './work'
 import * as L from '../logger'
 
-const recordNotFound = (logger: L.Logger) => (work: Work) => pipe(
-  TE.tryCatch(
-    async () => axios.patch(`http://commands:44001/works/${work.id}`, {
-      data: {
-        type: work.type,
-        id: work.id,
-        attributes: {
-          crossrefStatus: 'not-found',
+const recordNotFound = (logger: L.Logger) => (work: Work) => {
+  const url = `http://commands:44001/works/${work.id}`
+  return pipe(
+    TE.tryCatch(
+      async () => axios.patch(url, {
+        data: {
+          type: work.type,
+          id: work.id,
+          attributes: {
+            crossrefStatus: 'not-found',
+          },
         },
-      },
-    }, {
-      headers: { 'Content-Type': 'application/json' },
-    }),
-    (error) => logger.error('failed to update work', {
-      url: `http://commands:44001/works/${work.id}`,
-      work,
-      error,
-    }),
-  ),
-  TE.map(() => logger.info('work updated', { id: work.id, 'new-status': 'not-found' })),
-)
+      }, {
+        headers: { 'Content-Type': 'application/json' },
+      }),
+      (error) => logger.error('failed to update work', { url, work, error }),
+    ),
+    TE.map(() => logger.info('work updated', { id: work.id, 'new-status': 'not-found' })),
+  )
+}
 
 export const fetchMissingFrontMatter = async (logger: L.Logger): Promise<void> => {
   logger.info('fetchMissingFrontMatter starting')
