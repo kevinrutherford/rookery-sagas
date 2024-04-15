145d54c99715c5d169e35e1a42cfedb5b315adc5
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-15 11:35:31 +0100

    Split file to create expansion space #21

--- src/fetch-missing-front-matter/fetch-missing-front-matter.ts
+++ src/fetch-missing-front-matter/fetch-missing-front-matter.ts
@@ -2,35 +2,10 @@ import * as O from 'fp-ts/Option'
 import * as RA from 'fp-ts/ReadonlyArray'
 import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
+import { updateWork } from './update-work'
 import * as api from '../api'
-import { fetchCrossrefWork } from '../crossref/fetch-crossref-work'
 import { Saga } from '../invoke'
 import * as L from '../logger'
-import { Work } from '../resources/work'
-
-const updateWork = (logger: L.Logger) => (work: Work): Saga => pipe(
-  work,
-  fetchCrossrefWork(logger),
-  TE.mapLeft((fmr) => pipe(
-    {
-      type: work.type,
-      id: work.id,
-      attributes: {
-        crossrefStatus: 'not-determined',
-        reason: fmr.type,
-      },
-    },
-    api.updateWork(logger),
-  )),
-  TE.chain(api.updateWork(logger)),
-  TE.mapBoth(
-    (err) => ({
-      message: JSON.stringify(err),
-      payload: { err },
-    }),
-    () => null,
-  ),
-)
 
 export const fetchMissingFrontMatter = (logger: L.Logger): Saga => pipe(
   api.fetchWorksAwaitingFrontMatter(),
---
+++ src/fetch-missing-front-matter/update-work.ts
@@ -0,0 +1,32 @@
+import * as TE from 'fp-ts/TaskEither'
+import { pipe } from 'fp-ts/function'
+import * as api from '../api'
+import { fetchCrossrefWork } from '../crossref/fetch-crossref-work'
+import { Saga } from '../invoke'
+import * as L from '../logger'
+import { Work } from '../resources/work'
+
+export const updateWork = (logger: L.Logger) => (work: Work): Saga => pipe(
+  work,
+  fetchCrossrefWork(logger),
+  TE.mapLeft((fmr) => pipe(
+    {
+      type: work.type,
+      id: work.id,
+      attributes: {
+        crossrefStatus: 'not-determined',
+        reason: fmr.type,
+      },
+    },
+    api.updateWork(logger),
+  )),
+  TE.chain(api.updateWork(logger)),
+  TE.mapBoth(
+    (err) => ({
+      message: JSON.stringify(err),
+      payload: { err },
+    }),
+    () => null,
+  ),
+)
+
