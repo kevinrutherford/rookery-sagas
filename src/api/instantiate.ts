1bf4a05ecfda3fae83975ffcae7ec68645542dd9
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-15 15:24:43 +0100

    use the logger to report retries #21

--- src/api/instantiate.ts
+++ src/api/instantiate.ts
@@ -1,13 +1,19 @@
 import axios from 'axios'
 import axiosRetry, { exponentialDelay } from 'axios-retry'
+import { Logger } from '../logger'
 
-export const instantiate = (): void => {
+export const instantiate = (logger: Logger): void => {
   axiosRetry(axios, {
     retries: 3,
     retryCondition: () => true,
     retryDelay: exponentialDelay,
     onRetry: (retryCount: number, error) => {
-      process.stderr.write(`Axios retry -- retryCount: ${retryCount}, error: ${error}, url: ${error.config?.url}, data: ${error.config?.data}\n`)
+      logger.debug('Axios retry', {
+        retryCount,
+        error: JSON.stringify(error),
+        url: error.config?.url,
+        data: error.config?.data,
+      })
     },
   })
 }
