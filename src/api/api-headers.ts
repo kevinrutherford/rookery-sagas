1ebf925166af999895aa27370e32f822a426914f
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-01 10:54:16 +0100

    Extract a type to avoid circular dependencies later #28

---
+++ src/api/api-headers.ts
@@ -0,0 +1,2 @@
+export type ApiHeaders = Record<string, string>
+
--- src/api/update-work.ts
+++ src/api/update-work.ts
@@ -1,10 +1,9 @@
 import axios from 'axios'
 import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
+import { ApiHeaders } from './api-headers'
 import { FatalError } from '../invoke'
 import { UpdateWorkCommand } from '../resources/work'
-
-type ApiHeaders = Record<string, string>
 
 export const updateWork = (headers: ApiHeaders) => (cmd: UpdateWorkCommand): TE.TaskEither<FatalError, null> => {
   const url = `http://commands:44001/works/${cmd.id}`
