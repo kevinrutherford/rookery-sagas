d4bd865be26f90da8a1521b9b0895ef2ef1296d5
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-16 19:30:04 +0100

    Parse new events as they arrive

---
+++ src/outbox/domain-event.ts
@@ -0,0 +1,44 @@
+import * as t from 'io-ts'
+import * as tt from 'io-ts-types'
+
+const esEventBase = t.type({
+  id: tt.NonEmptyString,
+  created: tt.date,
+})
+
+const collectionCreatedEvent = t.intersection([esEventBase, t.type({
+  type: t.literal('collection-created'),
+  data: t.type({
+    id: t.string,
+    actorId: t.string,
+    name: t.string,
+    description: t.string,
+  }),
+})])
+
+const commentCreatedEvent = t.intersection([esEventBase, t.type({
+  type: t.literal('comment-created'),
+  data: t.type({
+    id: t.string,
+    actorId: t.string,
+    entryId: t.string,
+    content: t.string,
+  }),
+})])
+
+const doiEnteredEvent = t.intersection([esEventBase, t.type({
+  type: t.literal('discussion-started'),
+  data: t.type({
+    actorId: t.string,
+    entryId: t.string,
+    doi: t.string,
+    collectionId: t.string,
+  }),
+})])
+
+export const domainEvent = t.union([
+  collectionCreatedEvent,
+  commentCreatedEvent,
+  doiEnteredEvent,
+])
+
--- src/outbox/start.ts
+++ src/outbox/start.ts
@@ -1,10 +1,17 @@
 import { END, EventStoreDBClient, excludeSystemEvents } from '@eventstore/db-client'
+import * as E from 'fp-ts/Either'
+import { domainEvent } from './domain-event'
+import { Logger } from '../logger'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const propagate = (event: unknown): void => {
+const propagate = (logger: Logger) => (event: unknown): void => {
+  const e = domainEvent.decode(event)
+  if (E.isLeft(e))
+    return
+  logger.debug('Event received', { type: e.right.type })
 }
 
-export const start = (): void => {
+export const start = (logger: Logger): void => {
   const client = EventStoreDBClient.connectionString('esdb://eventstore:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000')
   const subscription = client.subscribeToAll({
     fromPosition: END,
@@ -15,7 +22,7 @@ export const start = (): void => {
     const event = resolvedEvent.event
     if (!event)
       return
-    propagate(event)
+    propagate(logger)(event)
   })
 }
 
