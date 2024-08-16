07791adfd16bf14ac01d526bd1bf0f1656d86541
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-16 19:15:31 +0100

    Subscribe to all new events

--- src/outbox/start.ts
+++ src/outbox/start.ts
@@ -1,3 +1,21 @@
+import { END, EventStoreDBClient, excludeSystemEvents } from '@eventstore/db-client'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const propagate = (event: unknown): void => {
+}
+
 export const start = (): void => {
+  const client = EventStoreDBClient.connectionString('esdb://eventstore:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000')
+  const subscription = client.subscribeToAll({
+    fromPosition: END,
+    filter: excludeSystemEvents(),
+  })
+
+  subscription.on('data', (resolvedEvent) => {
+    const event = resolvedEvent.event
+    if (!event)
+      return
+    propagate(event)
+  })
 }
 
