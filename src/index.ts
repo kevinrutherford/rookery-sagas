acdf4dc9648355af188fe48c6d5f07fdd5c6aca9
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-17 14:43:10 +0100

    Stay alive on fatal error

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,6 @@ const main = async (): Promise<void> => {
       saga,
       TE.mapLeft((fe) => {
         logger.error(fe.message, fe.payload)
-        logger.info('Terminating all sagas')
-        process.exit(1)
       }),
     )()
     logger.info('fetchMissingFrontMatter finished')
