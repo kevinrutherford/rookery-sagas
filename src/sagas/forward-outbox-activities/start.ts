7b28999d3c8aa8c31213dfbed9248fd053b86362
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-25 17:29:16 +0100

    Separate concerns

--- src/sagas/forward-outbox-activities/start.ts
+++ src/sagas/forward-outbox-activities/start.ts
@@ -15,19 +15,20 @@ const share = (api: Api, env: Config, event: CommentCreated) => {
 
 const isShareable = (env: Config) => (event: DomainEvent): boolean => Object.values(env).includes(event.data.actorId)
 
-const propagate = (api: Api, env: Config) => (event: DomainEvent): T.Task<void> => {
+type Listener = (event: DomainEvent) => T.Task<void>
+
+const propagate = (api: Api, env: Config): Listener => (event) => {
   if (isShareable(env)(event) && event.type === 'comment-created')
     return share(api, env, event)
   return async () => { }
 }
 
-export const start = (api: Api, vars: Config): void => {
+const dispatch = (listener: Listener): void => {
   const client = EventStoreDBClient.connectionString('esdb://eventstore:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000')
   const subscription = client.subscribeToAll({
     fromPosition: END,
     filter: excludeSystemEvents(),
   })
-
   subscription.on('data', async (resolvedEvent) => {
     const event = resolvedEvent.event
     if (!event)
@@ -36,8 +37,12 @@ export const start = (api: Api, vars: Config): void => {
       event,
       domainEvent.decode,
       TE.fromEither,
-      TE.chainTaskK(propagate(api, vars)),
+      TE.chainTaskK(listener),
     )()
   })
+}
+
+export const start = (api: Api, vars: Config): void => {
+  dispatch(propagate(api, vars))
 }
 
