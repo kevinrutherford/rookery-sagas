4128f6eadd6ad8e902128f615f164d56895cf3a5
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-25 19:27:49 +0100

    Clarify event type logic

--- src/sagas/forward-outbox-activities/forward-activity.ts
+++ src/sagas/forward-outbox-activities/forward-activity.ts
@@ -1,3 +1,4 @@
+import * as T from 'fp-ts/Task'
 import { Config } from './config'
 import { Api } from '../../api'
 import { renderCommentCreatedActivity } from '../activity-pub/render-comment-created-activity'
@@ -13,8 +14,12 @@ const share = (api: Api, env: Config, event: CommentCreated) => {
 const isShareable = (env: Config) => (event: DomainEvent): boolean => Object.values(env).includes(event.data.actorId)
 
 export const forwardActivity = (api: Api, env: Config): Listener => (event) => {
-  if (isShareable(env)(event) && event.type === 'comment-created')
-    share(api, env, event)
-  return async () => { }
+  switch (event.type) {
+    case 'comment-created':
+      if (isShareable(env)(event))
+        return share(api, env, event)
+    default:
+      return T.of(undefined)
+  }
 }
 
