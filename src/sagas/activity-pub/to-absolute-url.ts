dd49f95715ff8925f33b4ac48e57174ebf4858eb
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-28 16:03:52 +0100

    Include /api iff this is not localhost

--- src/sagas/activity-pub/to-absolute-url.ts
+++ src/sagas/activity-pub/to-absolute-url.ts
@@ -1,8 +1,13 @@
 import { Config } from '../forward-outbox-activities/config'
 
 export const toAbsoluteUrl = (env: Config) => (path: string): string => {
-  const host = env.ROOKERY_HOSTNAME.replace(new RegExp(/\/$/), '')
+  const host = env.ROOKERY_HOSTNAME
+    .replace(new RegExp(/\/$/), '')
+    .replace(/\/api$/, '')
   const resource = path.replace(new RegExp(/^\//), '')
-  return `${host}/${resource}`
+    .replace(/\/*api/, '')
+  return /localhost/.test(host)
+    ? `${host}/${resource}`
+    : `${host}/api/${resource}`
 }
 
--- test/sagas/activity-pub/to-absolute-url.test.ts
+++ test/sagas/activity-pub/to-absolute-url.test.ts
@@ -3,12 +3,12 @@ import { toAbsoluteUrl } from '../../../src/sagas/activity-pub/to-absolute-url'
 import { Config } from '../../../src/sagas/forward-outbox-activities/config'
 
 describe('toAbsoluteUrl', () => {
+
   describe.each([
     ['subdomain.example.com'],
     ['subdomain.example.com/'],
+    ['subdomain.example.com/api'],
     ['subdomain.example.com/api/'],
-    ['localhost:44002'],
-    ['localhost:44002/'],
   ])('given the ROOKERY_HOSTNAME "%s"', (hostname) => {
     const env: Config = {
       ROOKERY_HOSTNAME: hostname,
@@ -32,7 +32,47 @@ describe('toAbsoluteUrl', () => {
         expect(result).toMatch(/\//)
         expect(result).not.toMatch(/\/\//)
       })
+
+      it('includes /api', () => {
+        expect(result).toMatch(/\/api/)
+      })
     })
   })
+
+  describe.each([
+    ['localhost:44002'],
+    ['localhost:44002/'],
+    ['localhost:44002/api'],
+    ['localhost:44002/api/'],
+  ])('given the ROOKERY_HOSTNAME "%s"', (hostname) => {
+    const env: Config = {
+      ROOKERY_HOSTNAME: hostname,
+      USER_A1_ID: arbitraryWord(),
+      USER_A2_ID: arbitraryWord(),
+      USER_A3_ID: arbitraryWord(),
+      USER_CRB_ID: arbitraryWord(),
+    }
+
+    describe.each([
+      [arbitraryWord()],
+      ['/' + arbitraryWord()],
+    ])('and "%s" for the path', (path) => {
+      const result = toAbsoluteUrl(env)(path)
+
+      it('ends with the given path', () => {
+        expect(result).toMatch(new RegExp(`.*${path}$`))
+      })
+
+      it('has exactly one / before the path', () => {
+        expect(result).toMatch(/\//)
+        expect(result).not.toMatch(/\/\//)
+      })
+
+      it('does not include /api', () => {
+        expect(result).not.toMatch(/\/api/)
+      })
+    })
+  })
+
 })
 
